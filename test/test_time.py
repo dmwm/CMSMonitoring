3eccde748b276625c4a08a9f16ccabf7ee4bd378
Valentin Kuznetsov <vkuznet@gmail.com>
2019-01-25 10:17:23 -0500

    move option parser into global space, added avg time per iteration and time of single function call

--- test/test_time.py
+++ test/test_time.py
@@ -10,6 +10,7 @@ Description:
 # system modules
 import os
 import sys
+import time
 import json
 import argparse
 import timeit
@@ -23,22 +24,24 @@ class OptionParser():
         self.parser.add_argument("--doc", action="store",
             dest="doc", default="", help="Input file")
 
+optmgr  = OptionParser()
+opts = optmgr.parser.parse_args()
+doc = json.load(open(opts.doc))
+
 def test():
-    optmgr  = OptionParser()
-    opts = optmgr.parser.parse_args()
-    doc = json.load(open(opts.doc))
     result = validate_schema(doc, doc)
 
 def run():
     number = 1000
     res = timeit.timeit('test()', setup="from __main__ import test", number=number)
-    print("timeit: iteration {}, time {}".format(number, res))
+    print("timeit: iteration {}, time {}, avg time {}".format(number, res, res/number))
+    # single time
+    time0 = time.time()
+    res = validate_schema(doc, doc)
+    print("single function call time {}".format(time.time()-time0))
 
 def main():
     "Main function"
-#     optmgr  = OptionParser()
-#     opts = optmgr.parser.parse_args()
-#     doc = json.load(open(opts.doc))
     run()
 
 if __name__ == '__main__':
