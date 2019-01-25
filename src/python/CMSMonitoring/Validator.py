e5b0bbbaba90123164a6c842f9bdfaf8c849f4f8
Valentin Kuznetsov <vkuznet@gmail.com>
2019-01-25 09:53:14 -0500

    Add validate_schema function and class method; add appropriate unit test

--- src/python/CMSMonitoring/Validator.py
+++ src/python/CMSMonitoring/Validator.py
@@ -12,6 +12,34 @@ import os
 import sys
 import json
 
+def validate_schema(doc, schema):
+    "Validate schema of a given document"
+    base = 'SCHEMA_VALIDATOR'
+    if not isinstance(doc, dict):
+        return False
+    for key, val in doc.items():
+        if key not in schema:
+            print("{}: key={} is not in a schema".format(base, key))
+            return False
+        if isinstance(val, dict):
+            sub_schema = validate_schema(val, schema[key])
+            if not sub_schema:
+                print("{}: for sub schema={} val={} has wrong data-types".format(base, schema[key], val))
+                return False
+        expect = schema[key]
+        if isinstance(val, list):
+            types = set([type(x) for x in val])
+            if len(types) != 1:
+                print("{}: for key={} val={} has inconsisten data-types".format(base, key, val))
+                return False
+            if list(types)[0] != type(expect[0]):
+                print("{}: for key={} val={} has incorrect data-type in list, found {} expect {}".format(base, key, val, type(val), type(expect)))
+                return False
+        if type(val) != type(expect):
+            print("{}: for key={} val={} has incorrect data-type, found {} expect {}".format(base, key, val, type(val), type(expect)))
+            return False
+    return True
+
 class Validator(object):
     def __init__(self, schema):
         if not schema:
@@ -32,6 +60,10 @@ class Validator(object):
             if not self.validate(key, val):
                 return False
         return True
+
+    def validate_schema(self, doc):
+        "Validate schema of a given document"
+        return validate_schema(doc, self.schema)
 
 class ClassAdsValidator(Validator):
     def __init__(self, schema=None):
--- test/Validator_t.py
+++ test/Validator_t.py
@@ -12,7 +12,7 @@ import os
 import sys
 import unittest
 
-from CMSMonitoring.Validator import Validator, ClassAdsValidator
+from CMSMonitoring.Validator import Validator, ClassAdsValidator, validate_schema
 
 class testDASCore(unittest.TestCase):
     def setUp(self):
@@ -29,6 +29,24 @@ class testDASCore(unittest.TestCase):
         result = self.validator.validate('Pool', 'Bla')
         self.assertEqual(result, False)
         result = self.validator.validate('P', '')
+        self.assertEqual(result, False)
+
+    def testValidateSchema(self):
+        "Test validate_schema function"
+        schema = {'s':'s', 'i':1, 'd':{'ds':'s', 'di':1}, 'l':[1,2]}
+        result = validate_schema(schema, schema)
+        self.assertEqual(result, True)
+
+        doc = {'s': 1}
+        result = validate_schema(doc, schema)
+        self.assertEqual(result, False)
+
+        doc = {'s':'s', 'i':1, 'd':{'ds':'s', 'di':1}, 'l':[1,'2']}
+        result = validate_schema(doc, schema)
+        self.assertEqual(result, False)
+
+        doc = {'s':'s', 'i':1, 'd':{'ds':1, 'di':1}, 'l':[1,2]}
+        result = validate_schema(doc, schema)
         self.assertEqual(result, False)
 
 if __name__ == '__main__':
