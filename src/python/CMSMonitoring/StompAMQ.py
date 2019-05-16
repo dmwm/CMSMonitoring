9ff083f916231a07211d301e8cf1fa0b9c6de0b0
Valentin Kuznetsov <vkuznet@gmail.com>
2019-05-16 15:00:01 -0400

    Re-factor the code to make random connection to broker; keep them alive until we send all data and use random connection while submitting the data

--- src/python/CMSMonitoring/StompAMQ.py
+++ src/python/CMSMonitoring/StompAMQ.py
@@ -7,8 +7,11 @@ from __future__ import division
 
 import os
 import json
-import logging
 import time
+import socket
+import random
+import logging
+import traceback
 try:
     import stomp
 except ImportError:
@@ -92,6 +95,17 @@ class StompyListener(object):
 
         return (headers, body)
 
+def broker_ips(host, port):
+    "Return broker IP addresses from provide host name"
+    addr = []
+    for item in socket.getaddrinfo(host, int(port)):
+        # each item is (family, socktype, proto, canonname, sockaddr) tuple
+        # so we tack 4th element sockaddr
+        # for IPv4 sockaddr is (address, port)
+        # for IPv6 sockaddr is (address, port, flow info, scope id) 
+        # we are interested only in address
+        addr.append(item[4][0])
+    return addr
 
 class StompAMQ(object):
     """
@@ -111,6 +125,7 @@ class StompAMQ(object):
     :param cert: path to certificate file
     :param key: path to key file
     :param validation_loglevel: logging level to use for validation feedback
+    :param timeout_interval: provides timeout interval to failed broker
     """
 
     # Version number to be added in header
@@ -118,13 +133,26 @@ class StompAMQ(object):
 
     def __init__(self, username, password, producer, topic, validation_schema, 
                  host_and_ports=None, logger=None, cert=None, key=None,
-                 validation_loglevel=logging.WARNING):
+                 validation_loglevel=logging.WARNING,
+                 timeout_interval=600):
         self._username = username
         self._password = password
         self._producer = producer
         self._topic = topic
-        self._host_and_ports = host_and_ports or [('cms-test-mb.cern.ch', 61313)]
         self.logger = logger if logger else logging.getLogger()
+        self._host_and_ports = host_and_ports or [('cms-test-mb.cern.ch', 61313)]
+        self.ip_and_ports = []
+        try:
+            self.logger.info("host and ports", host_and_ports, type(host_and_ports))
+            if isinstance(host_and_ports, list):
+                for host, port in host_and_ports:
+                    for ipaddr in broker_ips(host, port):
+                        if (ipaddr, port) not in self.ip_and_ports:
+                            self.ip_and_ports.append((ipaddr, port))
+                self.logger.info("### resolver", self.ip_and_ports)
+        except Exception as exp:
+            traceback.print_exc()
+            self.logger.warn("unable to resolve host_and_ports", str(exp))
         self._cert = cert
         self._key = key
         self._use_ssl = True if key and cert else False
@@ -136,6 +164,68 @@ class StompAMQ(object):
 
         if self.validation_schema is None:
             logging.warning('No document validation performed!')
+
+        self.connections = []
+        if self.ip_and_ports:
+            for idx in range(len(self.ip_and_ports)):
+                host_and_ports = [self.ip_and_ports[idx]]
+                conn = stomp.Connection(host_and_ports=host_and_ports)
+                self.connections.append(conn)
+        else:
+            conn = stomp.Connection(host_and_ports=self._host_and_ports)
+            self.connections.append(conn)
+        self.timeouts = {}
+        self.timeout_interval = timeout_interval
+
+    def connect(self):
+        "Connect to the brokers"
+        available_connections = []
+        for conn in self.connections:
+            # check if we already connected, if so make it available and proceed
+            if conn.is_connected():
+                available_connections.append(conn)
+                continue
+
+            # perform connection to the broker
+            if self._use_ssl:
+                # This requires stomp >= 4.1.15
+                conn.set_ssl(for_hosts=self._host_and_ports, key_file=self._key, cert_file=self._cert)
+            conn.set_listener('StompyListener', StompyListener(self.logger))
+            # check if our connection failed before
+            # if so we'll wait until timeout_interval is passed
+            if conn in self.timeouts and \
+                    abs(self.timeouts[conn] - time.time()) < self.timeout_interval:
+                continue
+            try:
+                conn.start()
+                # If cert/key are used, ignore username and password
+                if self._use_ssl:
+                    conn.connect(wait=True)
+                else:
+                    conn.connect(username=self._username, passcode=self._password, wait=True)
+                available_connections.append(conn)
+                # we succeed to connect to broker, remove any record in timeout dict
+                if conn in self.timeouts:
+                    del self.timeouts[conn]
+                self.logger.debug("Connection to %s is successful", repr(self._host_and_ports))
+            except stomp.exception.ConnectFailedException as exc:
+                self.logger.error("Connection to %s failed %s", repr(self._host_and_ports), str(exc))
+                # record that our connection has failed
+                self.timeouts[conn] = time.time()
+
+        if not available_connections:
+            return None
+
+        # return random connection
+        idx = random.randint(0, len(available_connections)-1)
+        self.logger.debug("### available connections %s, con_id %s" % (len(available_connections), idx))
+        return available_connections[idx]
+
+    def disconnect(self):
+        "Disconnect from brokers"
+        for conn in self.connections:
+            if conn.is_connected():
+                conn.disconnect()
 
     def send(self, data):
         """
@@ -151,33 +241,15 @@ class StompAMQ(object):
         if isinstance(data, dict) and 'body' in data:
             data = [data]
 
-        conn = stomp.Connection(host_and_ports=self._host_and_ports)
-
-        if self._use_ssl:
-            # This requires stomp >= 4.1.15
-            conn.set_ssl(for_hosts=self._host_and_ports, key_file=self._key, cert_file=self._cert)
-
-        conn.set_listener('StompyListener', StompyListener(self.logger))
-        try:
-            conn.start()
-            # If cert/key are used, ignore username and password
-            if self._use_ssl:
-                conn.connect(wait=True)
-            else:
-                conn.connect(username=self._username, passcode=self._password, wait=True)
-
-        except stomp.exception.ConnectFailedException as exc:
-            self.logger.error("Connection to %s failed %s", repr(self._host_and_ports), str(exc))
-            return []
-
         failedNotifications = []
         for notification in data:
-            result = self._send_single(conn, notification)
-            if result:
-                failedNotifications.append(result)
+            conn = self.connect() # provide random connection to brokers
+            if conn:
+                result = self._send_single(conn, notification)
+                if result:
+                    failedNotifications.append(result)
 
-        if conn.is_connected():
-            conn.disconnect()
+        self.disconnect() # disconnect all available connections to brokers
 
         if failedNotifications:
             self.logger.warning('Failed to send to %s %i docs out of %i', repr(self._host_and_ports),
@@ -202,8 +274,8 @@ class StompAMQ(object):
                       ack='auto')
             self.logger.debug('Notification %s sent', str(notification))
         except Exception as exc:
-            self.logger.error('Notification: %s not send, error: %s', str(notification), str(exc))
-            return body
+            self.logger.error('Notification: %s (type=%s) not send, error: %s', \
+                    str(notification), type(notification), str(exc))
         return
 
     def make_notification(self, payload, docType, docId=None, producer=None, ts=None, metadata=None,
