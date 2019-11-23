f6a8798ac14859ce59df2c664f2c200f3f1fa4e5
Valentin Kuznetsov <vkuznet@gmail.com>
2019-11-23 15:58:22 -0500

    Add support for tls server; re-factor NATSManager and NATSManagerCmd

--- src/python/CMSMonitoring/NATS.py
+++ src/python/CMSMonitoring/NATS.py
@@ -21,6 +21,10 @@ Basic usage instructions:
     data = [...]   # list of dicts
     topics = [...] # list of publisher topics, e.g. cms-wma, T2
     attrs = [...]  # list of attributes to filter from data
+    # server can be in a form of 'nats://host:port' or
+    # nats with user/passwords 'nats://user:password@host:port'
+    # tls  with user/password  'tls://user:password@host:port'
+    server = 'nats://host:port'
     mgr = NATSManager(server, topics=topics, attrs=attrs, stdout=True)
     mgr.publish(data)
 """
@@ -85,7 +89,7 @@ class NATSManager(object):
         self.server = []
         self.sep = sep
         for srv in server.split(','):
-            if not srv.startswith('nats://'):
+            if not srv.startswith('nats://') or not srv.startswith('tls://'):
                 srv = 'nats://{}'.format(srv)
             if not srv.endswith(':4222'):
                 srv = '{}:4222'.format(srv)
@@ -145,20 +149,27 @@ class NATSManager(object):
         # always send all messages to default topic
         self.send(self.def_topic, cms_msgs)
 
-    def send(self, subject, msg):
-        "Call NATS function, user can pass either single message or list of messages"
-        if self.stdout:
-            if isinstance(msg, list):
-                for item in msg:
-                    print("{}: {}".format(subject, item))
-            else:
-                print("{}: {}".format(subject, msg))
+    def send_stdout(self, subject, msg):
+        "send subject/msg to stdout"
+        if isinstance(msg, list):
+            for item in msg:
+                print("{}: {}".format(subject, item))
         else:
-            if len(self.server) > 1:
-                server = self.server[random.randint(0, len(self.server)-1)]
-            else:
-                server = self.server[0]
+            print("{}: {}".format(subject, msg))
+
+    def get_server(self):
+        "Return random server from server pool"
+        if len(self.server) > 1:
+            return self.server[random.randint(0, len(self.server)-1)]
+        return self.server[0]
+
+    def send(self, subject, msg):
+        "send given message to subject topic"
+        if self.stdout:
+            self.send_stdout(subject, msg)
+        else:
             try:
+                server = self.get_server()
                 if NATS2:
                     tornado.ioloop.IOLoop.current().run_sync(lambda: nats(server, subject, msg))
                 else:
@@ -171,56 +182,41 @@ class NATSManager(object):
                 print("Failed to send docs to NATS, error: {}".format(str(exp)))
 
 
-def nats_pub(subject, msg, server=None, pub=None):
-    "NATS publisher via external NATS_PUB tool"
-    if not pub:
-        pub = os.getenv('NATS_PUB', '')
-        if not pub:
-            #print('No publication tool found, exit NATS...')
-            return
-    if not os.path.exists(pub):
-        print("Unable to locate {} on local file system".format(pub))
+def nats_cmd(cmd, server, subject, msg):
+    "NATS publisher via external nats cmd tool"
+    if not os.path.exists(cmd):
+        print("Unable to locate publisher tool '{}' on local file system".format(cmd))
         return
     if not server:
-        server = os.getenv('NATS_SERVER', '')
-        if not server:
-            #print('No NATS server found, exit NATS...')
-            return
-    cmd = '{} -s {} {} "{}"'.format(pub, server, subject, msg)
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, env=os.environ)
-    proc.wait()
-    return proc.returncode
+        print("No NATS server...")
+        return
+    if isinstance(msg, list):
+        for item in msg:
+            pcmd = '{} -s {} {} "{}"'.format(cmd, server, subject, msg)
+            proc = subprocess.Popen(pcmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, env=os.environ)
+            proc.wait()
+    else:
+        pcmd = '{} -s {} {} "{}"'.format(cmd, server, subject, msg)
+        proc = subprocess.Popen(pcmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, env=os.environ)
+        proc.wait()
+        return proc.returncode
 
-class NATSManager_pub(object):
+class NATSManagerCmd(NATSManager):
     "NATS manager based on external publisher tool"
-    def __init__(self, server=None, pub=None, topics=None, cms_filter=None):
-        self.topics = topics
-        self.server = server
-        self.pub = pub
-        self.cms_filter = cms_filter if cms_filter else def_filter
+    def __init__(self, cmd, server=None, topics=None, attrs=None, sep='   ', default_topic='cms-wma', stdout=False, cms_filter=None):
+        self.pub = cmd
+        super(NATSManagerCmd, self).__init__(server, topics, attrs, sep, default_topic, stdout, cms_filter)
 
-    def publish(self, data):
-        "Publish given set of docs to topics"
-        if not self.topics:
-            # we will use input docs to get list of sites
-            for doc in data:
-                for rec in self.cms_filter(doc):
-                    subject = rec.get('site', '')
-                    if not subject:
-                        continue
-                    nats_pub(subject, nats_encoder(rec), server=self.server, pub=self.pub)
-                    # send to default cms-wma topic too
-                    nats_pub('cms-wma', nats_encoder(rec), server=self.server, pub=self.pub)
-            return
-        for topic in self.topics:
-            pat = re.compile(topic)
-            for doc in data:
-                for rec in self.cms_filter(doc):
-                    msg = nats_encoder(rec)
-                    if msg.find(topic) != -1 or pat.match(msg):
-                        nats_pub(topic, nats_encoder(rec), server=self.server, pub=self.pub)
-                    # send to cms-wma by default
-                    nats_pub('cms-wma', nats_encoder(rec), server=self.server, pub=self.pub)
+    def __repr__(self):
+        return 'NATSManagerCmd@{}, servers={} topics={} def_topic={} attrs={} cms_filter={} stdout={}'.format(hex(id(self)), self.server, self.topics, self.def_topic, self.attrs, self.cms_filter, self.stdout)
+
+    def send(self, subject, msg):
+        "Call NATS function, user can pass either single message or list of messages"
+        if self.stdout:
+            self.send_stdout(subject, msg)
+        else:
+            server = self.get_server()
+            nats_cmd(self.cmd, server, subject, msg)
 
 def test():
     "Test function"
