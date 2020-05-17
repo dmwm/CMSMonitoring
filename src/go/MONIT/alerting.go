f81b424f77df937c7552ec33bb1d831a9c83e653
indrarahul2013 <indrarahul2013@gmail.com>
2020-05-18 01:49:55 +0530

    Updated alerting.go (AlertManager REST API)

--- src/go/MONIT/alerting.go
+++ src/go/MONIT/alerting.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"flag"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
-	"os/exec"
 	"time"
 )
+
+// File       : alerting.go
+// Author     : Rahul Indra <indrarahul2013 AT gmail dot com>
+// Created    : Thu, 16 May 2020 16:45:00 GMT
+// Description: Alerting Module for CERN MONIT infrastructure
 
 //URL for AlertManager
 var alertManagerURL string
 
-//Severity of alerts
+//severity of alerts
 var severity string
+
+//verbose defines verbosity level
+var verbose int
 
 //CERN SSB Data Struct
 type ssb struct {
@@ -26,6 +35,31 @@ type ssb struct {
 		} `json:"series"`
 		StatementID int `json:"statement_id"`
 	} `json:"results"`
+}
+
+//AlertManager API acceptable JSON Data for CERN SSB Data
+type amJSON struct {
+	Labels struct {
+		Alertname string `json:"alertname"`
+		Severity  string `json:"severity"`
+	} `json:"labels"`
+	Annotations struct {
+		Date             string `json:"date"`
+		Description      string `json:"description"`
+		FeName           string `json:"fe_name"`
+		MonitState       string `json:"monit_state"`
+		MonitState1      string `json:"monit_state_1"`
+		SeName           string `json:"se_name"`
+		ShortDescription string `json:"short_description"`
+		SsbNumber        string `json:"ssb_number"`
+		SysCreatedBy     string `json:"sys_created_by"`
+		SysModCount      string `json:"sys_mod_count"`
+		SysUpdatedBy     string `json:"sys_updated_by"`
+		Type             string `json:"type"`
+		UpdateTimestamp  string `json:"update_timestamp"`
+	} `json:"annotations"`
+	StartsAt string `json:"startsAt"`
+	EndsAt   string `json:"endsAt"`
 }
 
 //function for parsing JSON data from CERN SSB Data
@@ -45,68 +79,19 @@ func fetchJSON(filename string) []byte {
 		log.Printf("Unable to read JSON file, error: %v\n", err)
 	}
 
-	return jsonData
-
-}
-
-//function for fetching JSON data from CERN SSB Data monit.go InfluxDB query.
-func fetchJSONmonit() []byte {
-	token := os.Getenv("token")
-	cmd := exec.Command("go", "run", "monit.go", "-query="+"select * from outages where time > now() - 2h limit 1", "-dbname=monit_production_ssb_otgs", "-token="+token, "-dbid=9474")
-	jsonData, err := cmd.Output()
-
-	if err != nil {
-		log.Printf("Unable to run monit command, error: %v\n", err)
-
+	if verbose > 1 {
+		log.Println("SSB Data : \n" + string(jsonData))
 	}
 
 	return jsonData
-}
-
-//helper function for amtool.
-func amtool(alertname,
-	start,
-	end,
-	date,
-	description,
-	fe_name,
-	monit_state,
-	monit_state_1,
-	se_name,
-	short_description,
-	ssb_number,
-	sys_created_by,
-	sys_mod_count,
-	sys_updated_by,
-	a_type,
-	update_timestamp string) {
-
-	cmd := exec.Command("amtool", "alert", "add", alertname, "severity="+severity,
-		"--annotation=date="+date,
-		"--annotation=description="+description,
-		"--annotation=fe_name="+fe_name,
-		"--annotation=monit_state="+monit_state,
-		"--annotation=monit_state_1="+monit_state_1,
-		"--annotation=se_name="+se_name,
-		"--annotation=short_description="+short_description,
-		"--annotation=ssb_number="+ssb_number,
-		"--annotation=sys_created_by="+sys_created_by,
-		"--annotation=sys_mod_count="+sys_mod_count,
-		"--annotation=sys_updated_by="+sys_updated_by,
-		"--annotation=type="+a_type,
-		"--annotation=update_timestamp="+update_timestamp,
-		"--start", start, "--end", end, "--alertmanager.url", alertManagerURL)
-	_, err := cmd.Output()
-
-	if err != nil {
-		log.Printf("Unable to run amtool command, error: %v\n", err)
-		return
-	}
 
 }
 
-//function for pushing each data from JSON file to AlertManager.
-func (data *ssb) pushData() {
+//function for converting SSB JSON Data into JSON Data required by AlertManager APIs.
+func (data *ssb) convertData() []byte {
+
+	var temp amJSON
+	var finalData []amJSON
 
 	for _, each := range data.Results[0].Series[0].Values {
 
@@ -118,46 +103,87 @@ func (data *ssb) pushData() {
 		_endRFC3339 := time.Unix(0, end*int64(time.Millisecond)).UTC().Format(time.RFC3339)
 		_updatetsRFC3339 := time.Unix(0, updatets*int64(time.Millisecond)).UTC().Format(time.RFC3339)
 
-		alertname := each[2].(string)
-		date := each[0].(string)
-		description := each[2].(string)
-		fe_name := each[5].(string)
-		monit_state := each[6].(string)
-		monit_state_1 := each[7].(string)
-		se_name := each[8].(string)
-		short_description := each[9].(string)
-		ssb_number := each[10].(string)
-		sys_created_by := each[11].(string)
-		sys_mod_count := each[12].(string)
-		sys_updated_by := each[13].(string)
-		a_type := each[14].(string)
+		temp.Labels.Alertname = each[2].(string)
+		temp.Labels.Severity = severity
 
-		amtool(alertname, _beginRFC3339, _endRFC3339, date,
-			description, fe_name, monit_state, monit_state_1, se_name,
-			short_description, ssb_number, sys_created_by, sys_mod_count,
-			sys_updated_by, a_type, _updatetsRFC3339)
+		temp.Annotations.Date = each[0].(string)
+		temp.Annotations.Description = each[2].(string)
+		temp.Annotations.FeName = each[5].(string)
+		temp.Annotations.MonitState = each[6].(string)
+		temp.Annotations.MonitState1 = each[7].(string)
+		temp.Annotations.SeName = each[8].(string)
+		temp.Annotations.ShortDescription = each[9].(string)
+		temp.Annotations.SsbNumber = each[10].(string)
+		temp.Annotations.SysCreatedBy = each[11].(string)
+		temp.Annotations.SysModCount = each[12].(string)
+		temp.Annotations.SysUpdatedBy = each[13].(string)
+		temp.Annotations.Type = each[14].(string)
+		temp.Annotations.UpdateTimestamp = _updatetsRFC3339
+
+		temp.StartsAt = _beginRFC3339
+		temp.EndsAt = _endRFC3339
+
+		finalData = append(finalData, temp)
+
 	}
 
+	jsonStr, err := json.Marshal(finalData)
+
+	if err != nil {
+		log.Printf("Unable to convert JSON Data, error: %v\n", err)
+	}
+
+	return jsonStr
+
+}
+
+//function for making post request on /api/v1/alerts alertmanager endpoint for creating alerts.
+func post(jsonStr []byte) {
+	apiurl := alertManagerURL + "/api/v1/alerts"
+
+	req, err := http.NewRequest("POST", apiurl, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if verbose >= 1 {
+		log.Println("response Status:", resp.Status)
+		log.Println("response Headers:", resp.Header)
+		body, _ := ioutil.ReadAll(resp.Body)
+		log.Println("response Body:", string(body))
+	}
 }
 
 func main() {
 
 	var in string
-	alertManagerURL = "http://localhost:9093"
 	severity = "monitoring"
 
 	flag.StringVar(&in, "input", "", "input filename")
+	flag.StringVar(&alertManagerURL, "url", "", "alertmanager URL")
+	flag.IntVar(&verbose, "verbose", 0, "verbosity level")
 	flag.Parse()
 
 	if in == "" {
 		log.Fatalf("Input filename missing. Exiting....")
 	}
 
+	if alertManagerURL == "" {
+		log.Fatalf("AlertManager URL missing. Exiting....")
+	}
+
 	jsonData := fetchJSON(in)
-	// jsonData := fetchJSONmonit()
 
 	var data ssb
 	data.parseJSON(jsonData)
-	data.pushData()
+
+	jsonStrAM := data.convertData() //JSON data in AlertManager APIs format.
+
+	post(jsonStrAM)
 
 }
