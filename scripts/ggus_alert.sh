2fc0ac1da96e73b04d972198e63342363c95a408
indrarahul2013 <indrarahul2013@gmail.com>
2020-05-22 18:56:53 +0530

    Updated ggus_alert_manage+ggus_alert.sh

--- scripts/ggus_alert.sh
+++ scripts/ggus_alert.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 ##H Script for fetching GGUS Tickets and injecting them into MONIT (AlertManager)
-##H Usage: ggus_alerts.sh <ggus_format> [url] [interval] [level]"
+##H Usage: ggus_alerts.sh [ggus_format] [cmsmon_url] [interval] [vo] [timeout] [verbose]"
 ##H
 ##H Options:
-##H   url        alertmanager url (default: https://cms-monitoring.cern.ch)
-##H   interval   time interval (in sec) at which fetching and injecting data repeats (default: 1)
-##H   verbose    verbosity level (int) (default: 0)
+##H   ggus_format   GGUS Query Format ("csv" or "xml")              (default: "csv")
+##H   cmsmon_url    CMS Monitoring URL                              (default: https://cms-monitoring.cern.ch)
+##H   interval      Time interval for Alerts ingestion & injection  (default: 1)
+##H   vo            Required VO attribute                           (default: "cms")
+##H   timeout       HTTP client timeout operation (GGUS Parser)     (default:0 - zero means no timeout)
+##H   verbose       Verbosity level                                 (default: 0)
 ##H
 
 # Check if user is passing least required arguments.
@@ -17,16 +20,22 @@ fi
 
 ggus_format=${1:-"csv"}
 
-# Alerting Tool optional arguments
-alertmanager_url=${2:-"https://cms-monitoring.cern.ch"}
+## Alerting Tool optional arguments
+cmsmon_url=${2:-"https://cms-monitoring.cern.ch"}
 interval=${3:-1}
 vo=${4:-"cms"}
-verbose=${5:-0}
+##
 
-input_file=data.json
+## GGUS Parser optional arguments
+timeout=${5:-0}
+##
+
+verbose=${6:-0}
+
+data_file=data.json
 
 while true;  do
-   ggus_parser -format $ggus_format -out $input_file
-   ggus_alerting -input $input_file -url $alertmanager_url -vo $vo -verbose $verbose
+   ggus_parser -format $ggus_format -out $data_file -timeout $timeout -verbose $verbose
+   ggus_alerting -input $data_file -url $cmsmon_url -vo $vo -verbose $verbose
    sleep $interval
 done
