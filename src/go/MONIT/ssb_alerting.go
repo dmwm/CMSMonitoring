b01f5a0986fe190d2317c8aadf7d9c0a9d84ec4a
indrarahul2013 <indrarahul2013@gmail.com>
2020-07-08 13:17:40 +0530

    changed SSB default severity level to 'notification'

--- src/go/MONIT/ssb_alerting.go
+++ src/go/MONIT/ssb_alerting.go
@@ -351,7 +351,7 @@ func alert(inp string) {
 func main() {
 
 	var inp string
-	severity = "info" // TODO: repalce with severity level from the annotations
+	severity = "notification" // TODO: repalce with severity level from the annotations
 	tag = "monitoring"
 	service = "SSB"
 
