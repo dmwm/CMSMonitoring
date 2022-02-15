629baefd5f293ac831291a7e87d467443466b03d
mrceyhun <ceyhunuzngl@gmail.com>
2022-02-15 22:52:10 +0100

    Fix ggus_alerting parameter

--- scripts/ggus_alert.sh
+++ scripts/ggus_alert.sh
@@ -37,7 +37,7 @@ verbose=${7:-0}
 while true;  do
     echo "### ggus_parser -format $ggus_format -out $data_file -timeout $timeout -verbose $verbose"
     ggus_parser -format $ggus_format -out $data_file -timeout $timeout -verbose $verbose
-    echo "### ggus_alerting -input $data_file -url $cmsmon_url -vo $vo -verbose $verbose"
-    ggus_alerting -input $data_file -url $cmsmon_url -vo $vo -verbose $verbose
+    echo "### ggus_alerting -input $data_file -urls $cmsmon_url -vo $vo -verbose $verbose"
+    ggus_alerting -input $data_file -urls $cmsmon_url -vo $vo -verbose $verbose
     sleep $interval
 done
