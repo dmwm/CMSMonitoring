a44931682c8412d64939c0547d4a5a7b1e4c3b78
Ceyhun <ceyhunuzngl@gmail.com>
2022-03-01 23:24:13 +0100

    Silence noisy stdout of rucio_quotas (#133)

--- scripts/cron4rucio_quotas.sh
+++ scripts/cron4rucio_quotas.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-source /cvmfs/cms.cern.ch/rucio/setup-py3.sh
-voms-proxy-init -voms cms -rfc -valid 192:00
+source /cvmfs/cms.cern.ch/cmsset_default.sh >/dev/null
+source /cvmfs/cms.cern.ch/rucio/setup-py3.sh >/dev/null
+voms-proxy-init -voms cms -rfc -valid 192:00 >/dev/null
 
 script_dir="$(
     cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit
