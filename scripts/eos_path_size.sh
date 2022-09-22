34ef00aa534d701534c0ad44d13eee8742f6ffe2
mrceyhun <ceyhunuzngl@gmail.com>
2022-09-22 19:00:55 +0200

    Implement new eos quotas page with ec and non ec tables

--- scripts/eos_path_size.sh
+++ scripts/eos_path_size.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-##H Script to create CMS Eos path sizes with conditons
+##H Script to create CMS Eos path sizes with conditions
 ##H CMSVOC and CMSMONITORING groups are responsible for this script.
 
-. /cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-centos7-gcc8-opt/setup.sh
+. /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc8-opt/setup.sh
 
 # Catch output to not print successful jobs stdout to email, print when failed
 if ! output=$(pip install --user schema 2>&1); then
@@ -10,32 +10,26 @@ if ! output=$(pip install --user schema 2>&1); then
     exit $?
 fi
 
-# Check xrdcp executable exists
-if ! [ -x "$(command -v xrdcp)" ]; then
-    echo "It seems xrdcp is not exist in PATH! Exiting..."
-    exit 1
-fi
-
 if ! [ "$(python -c 'import sys; print(sys.version_info.major)')" = 3 ]; then
     echo "It seem python version is not 3.X! Exiting..."
     exit 1
 fi
 
-script_dir="$(
+script_dir=$(
     cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit
     pwd -P
-)"
+)
 
 py_input_args=(
     --output_file "/eos/user/c/cmsmonit/www/eos-path-size/size.html"
-    --input_eos_file "/eos/cms/store/accounting/eos_quota_ls.txt"
+    --non_ec_json "/eos/cms/store/accounting/eos_non_ec_accounting.json"
+    --ec_json "/eos/cms/store/accounting/eos_ec_accounting.json"
     --static_html_dir "${script_dir}/../src/html/eos_path_size"
 )
 
 # Catch	output to not print successful jobs stdout to email, print when failed
 if ! output=$(
-    xrdcp -s root://eoscms.cern.ch//eos/cms/proc/accounting - |
-        python "$HOME"/CMSMonitoring/src/python/CMSMonitoring/eos_path_size.py "${py_input_args[@]}" 2>&1
+    python "$HOME"/CMSMonitoring/src/python/CMSMonitoring/eos_path_size.py "${py_input_args[@]}" 2>&1
 ); then
     echo "$output"
     exit $?
