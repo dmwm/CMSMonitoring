9c674d4314403ca78305132727411f7e1997d544
mrceyhun <ceyhunuzngl@gmail.com>
2022-09-29 16:16:53 +0200

    Replace hardcoded jdbc url to be fetched from secret in sqoop pod

--- sqoop/scripts/rucio_table_dumps.sh
+++ sqoop/scripts/rucio_table_dumps.sh
@@ -4,7 +4,6 @@ set -e
 
 # Imports CMS_RUCIO_PROD tables
 BASE_PATH=/project/awg/cms/rucio/
-JDBC_URL=jdbc:oracle:thin:@cms-nrac-scan.cern.ch:10121/CMSR_CMS_NRAC.cern.ch
 SCHEMA="CMS_RUCIO_PROD"
 # Suggested order
 RUCIO_TABLES="replicas dids contents dataset_locks locks rules rules_history rses"
@@ -24,6 +23,7 @@ onFailExit() {
 if [ -f /etc/secrets/rucio ]; then
     USERNAME=$(grep username </etc/secrets/rucio | awk '{print $2}')
     PASSWORD=$(grep password </etc/secrets/rucio | awk '{print $2}')
+    JDBC_URL=$(grep jdbc_url </etc/secrets/rucio | awk '{print $2}')
 else
     echo "[ERROR] Unable to read Rucio credentials" >>"$LOG_FILE".stdout
     exit 1
