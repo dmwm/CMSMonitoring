efc77abba834e363d6f0b24463cc1085f5ac70de
mrceyhun <ceyhunuzngl@gmail.com>
2022-11-08 19:54:17 +0100

    Fix pushgateway metric names for custom dbs table dumps

--- sqoop/scripts/cms-dbs3-files.sh
+++ sqoop/scripts/cms-dbs3-files.sh
@@ -9,7 +9,9 @@ BASE_PATH=$(util_get_config_val "$myname")
 START_TIME=$(date +%s)
 SCHEMA="CMS_DBS3_PROD_GLOBAL_OWNER"
 LOG_FILE=log/$(date +'%F_%H%M%S')_$myname
-pushg_dump_start_time "$myname" "DBS" "$SCHEMA" "FILES_custom"
+# pushgateway table name
+custom_pg_table_name="FILES_custom"
+pushg_dump_start_time "$myname" "DBS" "$SCHEMA" "$custom_pg_table_name"
 
 # --------------------------------------------------------------------------------- START
 JDBC_URL=$(sed '1q;d' /etc/secrets/cmsr_cstring)
@@ -55,5 +57,5 @@ fi
 # ---------------------------------------------------------------------------- STATISTICS
 duration=$(($(date +%s) - START_TIME))
 pushg_dump_duration "$myname" "DBS" "$SCHEMA" $duration
-pushg_dump_end_time "$myname" "DBS" "$SCHEMA" "FILES"
+pushg_dump_end_time "$myname" "DBS" "$SCHEMA" "$custom_pg_table_name"
 util4logi "all finished, time spent: $(util_secs_to_human $duration)" >>"$LOG_FILE".stdout
