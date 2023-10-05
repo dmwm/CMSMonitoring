caaddfaf54c2f6efb8ca9bdf2132c47c16e7c445
Nikodemas Tuckus <ntuckus@gmail.com>
2023-10-05 17:06:34 +0200

    Change signature and formatting

--- src/python/CMSMonitoring/amq_sender.py
+++ src/python/CMSMonitoring/amq_sender.py
@@ -4,6 +4,7 @@ import time
 import logging
 from StompAMQ7 import StompAMQ7
 
+
 def credentials(f_name):
     if os.path.exists(f_name):
         return json.load(open(f_name))
@@ -21,29 +22,41 @@ def drop_nulls_in_dict(d):  # d: dict
 def to_chunks(data, samples=1000):
     length = len(data)
     for i in range(0, length, samples):
-        yield data[i:i + samples]
+        yield data[i : i + samples]
 
 
-def send_to_amq(data, confs, batch_size):
+def send_to_amq(data, confs, batch_size, topic=None, doc_type=None):
     """Sends list of dictionary in chunks"""
     wait_seconds = 0.001
     if confs:
-        username = confs.get('username', '')
-        password = confs.get('password', '')
-        producer = confs.get('producer')
-        doc_type = confs.get('type', None)
-        topic = confs.get('topic')
-        host = confs.get('host')
-        port = int(confs.get('port'))
-        cert = confs.get('cert', None)
-        ckey = confs.get('ckey', None)
+        username = confs.get("username", "")
+        password = confs.get("password", "")
+        producer = confs.get("producer")
+        if not doc_type:
+            doc_type = confs.get("type")
+        if not topic:
+            topic = confs.get("topic")
+        host = confs.get("host")
+        port = int(confs.get("port"))
+        cert = confs.get("cert")
+        ckey = confs.get("ckey")
         for chunk in to_chunks(data, batch_size):
-            stomp_amq = StompAMQ7(username=username, password=password, producer=producer, topic=topic,
-                                  key=ckey, cert=cert, validation_schema=None, host_and_ports=[(host, port)],
-                                  loglevel=logging.WARNING)
+            stomp_amq = StompAMQ7(
+                username=username,
+                password=password,
+                producer=producer,
+                topic=topic,
+                key=ckey,
+                cert=cert,
+                validation_schema=None,
+                host_and_ports=[(host, port)],
+                loglevel=logging.WARNING,
+            )
             messages = []
             for msg in chunk:
-                notif, _, _ = stomp_amq.make_notification(payload=msg, doc_type=doc_type, producer=producer)
+                notif, _, _ = stomp_amq.make_notification(
+                    payload=msg, doc_type=doc_type, producer=producer
+                )
                 messages.append(notif)
             if messages:
                 stomp_amq.send(messages)
