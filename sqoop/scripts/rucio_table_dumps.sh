330a1eb9ea066efc2fe8f6cc9948a751457a3449
nikodemas <47255905+nikodemas@users.noreply.github.com>
2025-01-31 11:47:53 +0100

    Add rucio table to sqoop dump

--- sqoop/scripts/rucio_table_dumps.sh
+++ sqoop/scripts/rucio_table_dumps.sh
@@ -7,7 +7,7 @@ set -e
 TZ=UTC
 SCHEMA="CMS_RUCIO_PROD"
 # Suggested order
-RUCIO_TABLES="replicas dids contents dataset_locks locks rules rules_history requests_history subscriptions rses accounts account_limits bad_replicas deleted_dids"
+RUCIO_TABLES="replicas dids contents dataset_locks locks rules rules_history requests_history subscriptions rses accounts account_limits bad_replicas deleted_dids rse_attr_map"
 
 myname=$(basename "$0")
 BASE_PATH=$(util_get_config_val "$myname")
